8eadaab319d3bd5bd92d3ed46a7030aa03f1e977
Shafira Putri Novia Hartanti <79043591+shafiraputri01@users.noreply.github.com>
2022-09-26 23:44:02 +0700

    Update ace04.sh

--- scripts/mrc_ner/reproduce/ace04.sh
+++ scripts/mrc_ner/reproduce/ace04.sh
@@ -26,7 +26,7 @@ MAX_EPOCH=20
 SPAN_CANDI=pred_and_gold
 PROGRESS_BAR=1
 
-OUTPUT_DIR=/userhome/xiaoya/outputs/mrc_ner/${TIME}/ace2004/large_lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}
+OUTPUT_DIR=/content/outputs/mrc_ner/${TIME}/ace2004/large_lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}
 mkdir -p ${OUTPUT_DIR}
 
 CUDA_VISIBLE_DEVICES=0,1 python ${REPO_PATH}/train/mrc_ner_trainer.py \
@@ -43,7 +43,7 @@ CUDA_VISIBLE_DEVICES=0,1 python ${REPO_PATH}/train/mrc_ner_trainer.py \
 --val_check_interval ${VAL_CKPT} \
 --accumulate_grad_batches ${ACC_GRAD} \
 --default_root_dir ${OUTPUT_DIR} \
---mrc_dropout ${MRC_DROPOUT}\
+--mrc_dropout ${MRC_DROPOUT} \
 --bert_dropout ${BERT_DROPOUT} \
 --max_epochs ${MAX_EPOCH} \
 --span_loss_candidates ${SPAN_CANDI} \
