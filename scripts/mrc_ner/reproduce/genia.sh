f55cdcbdbb1988d005bc146c910642b467a586a0
Shafira Putri Novia Hartanti <79043591+shafiraputri01@users.noreply.github.com>
2022-10-10 21:22:04 +0700

    Update genia.sh

--- scripts/mrc_ner/reproduce/genia.sh
+++ scripts/mrc_ner/reproduce/genia.sh
@@ -6,31 +6,31 @@
 REPO_PATH=/userhome/xiaoya/mrc-for-flat-nested-ner
 export PYTHONPATH="$PYTHONPATH:$REPO_PATH"
 
-DATA_DIR=/userhome/xiaoya/dataset/genia
-BERT_DIR=/userhome/xiaoya/bert/bert_cased_large
-BERT_DROPOUT=0.2
-MRC_DROPOUT=0.2
-LR=2e-5
+DATA_DIR=/content/dataset/genia
+BERT_DIR=/content/bert/bert_cased_large
+BERT_DROPOUT=0.1
+MRC_DROPOUT=0.3
+LR=4e-5
 SPAN_WEIGHT=0.1
 WARMUP=0
-MAXLEN=180
+MAXLEN=128
 MAXNORM=1.0
 INTER_HIDDEN=2048
 
 BATCH_SIZE=8
 PREC=16
 VAL_CKPT=0.25
-ACC_GRAD=4
-MAX_EPOCH=20
+ACC_GRAD=2
+MAX_EPOCH=15
 SPAN_CANDI=pred_and_gold
 PROGRESS_BAR=1
-WEIGHT_DECAY=0.002
+WEIGHT_DECAY=0.01
 
-OUTPUT_DIR=/userhome/xiaoya/outputs/github_mrc/genia/large_lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_bsz32_hard_span_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}
+OUTPUT_DIR=/content/outputs/github_mrc/genia/large_lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_bsz32_hard_span_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}
 mkdir -p ${OUTPUT_DIR}
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ${REPO_PATH}/train/mrc_ner_trainer.py \
---gpus="4" \
+CUDA_VISIBLE_DEVICES=0 python ${REPO_PATH}/train/mrc_ner_trainer.py \
+--gpus="1" \
 --distributed_backend=ddp \
 --workers 0 \
 --data_dir ${DATA_DIR} \
