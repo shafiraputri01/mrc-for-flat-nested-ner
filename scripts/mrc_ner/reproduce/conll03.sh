1d356d3bea0927f8d6ae09192fe4d90c352a2fa0
Shafira Putri Novia Hartanti <79043591+shafiraputri01@users.noreply.github.com>
2022-10-10 21:17:00 +0700

    Update conll03.sh

--- scripts/mrc_ner/reproduce/conll03.sh
+++ scripts/mrc_ner/reproduce/conll03.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-
 TIME=0901
 FILE=conll03_cased_large
-REPO_PATH=/userhome/xiaoya/mrc-for-flat-nested-ner
+REPO_PATH=/content/mrc-for-flat-nested-ner
 export PYTHONPATH="$PYTHONPATH:$REPO_PATH"
 
-DATA_DIR=/userhome/xiaoya/dataset/en_conll03
-BERT_DIR=/userhome/xiaoya/bert/bert_cased_large
-OUTPUT_BASE=/userhome/xiaoya/outputs
+DATA_DIR=/content/dataset/conll03
+BERT_DIR=/content/bert/bert_cased_large
+OUTPUT_BASE=/content/outputs
 
-BATCH=10
-GRAD_ACC=4
+BATCH=8
+GRAD_ACC=2
 BERT_DROPOUT=0.1
 MRC_DROPOUT=0.3
-LR=3e-5
-LR_MINI=3e-7
+LR=4e-5
+LR_MINI=5e-7
 LR_SCHEDULER=polydecay
 SPAN_WEIGHT=0.1
 WARMUP=0
-MAX_LEN=200
+MAX_LEN=128
 MAX_NORM=1.0
-MAX_EPOCH=20
+MAX_EPOCH=15
 INTER_HIDDEN=2048
 WEIGHT_DECAY=0.01
 OPTIM=torch.adam
-VAL_CHECK=0.2
+VAL_CHECK=0.25
 PREC=16
 SPAN_CAND=pred_and_gold
 
@@ -35,12 +34,12 @@ OUTPUT_DIR=${OUTPUT_BASE}/mrc_ner/${TIME}/${FILE}_cased_large_lr${LR}_drop${MRC_
 mkdir -p ${OUTPUT_DIR}
 
 
-CUDA_VISIBLE_DEVICES=0,1 python ${REPO_PATH}/train/mrc_ner_trainer.py \
+CUDA_VISIBLE_DEVICES=0 python ${REPO_PATH}/train/mrc_ner_trainer.py \
 --data_dir ${DATA_DIR} \
 --bert_config_dir ${BERT_DIR} \
 --max_length ${MAX_LEN} \
 --batch_size ${BATCH} \
---gpus="2" \
+--gpus="1" \
 --precision=${PREC} \
 --progress_bar_refresh_rate 1 \
 --lr ${LR} \
