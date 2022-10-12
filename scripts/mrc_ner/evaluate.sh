04ec84d12e4a668c5f42c5457061e484530e8b2b
Shafira Putri Novia Hartanti <79043591+shafiraputri01@users.noreply.github.com>
2022-10-12 12:01:08 +0700

    Update evaluate.sh

--- scripts/mrc_ner/evaluate.sh
+++ scripts/mrc_ner/evaluate.sh
@@ -3,13 +3,13 @@
 
 # file: eval.sh
 
-REPO_PATH=/data/xiaoya/workspace/mrc-for-flat-nested-ner
+REPO_PATH=/content/mrc-for-flat-nested-ner
 export PYTHONPATH="$PYTHONPATH:$REPO_PATH"
 
-OUTPUT_DIR=/data/xiaoya/outputs/mrc_ner/ace2004/debug_lr3e-5_drop0.3_norm1.0_weight0.1_warmup0_maxlen100
+OUTPUT_DIR=/content/outputs/mrc_ner/0910/ace2004/large_lr4e-5_drop0.3_norm1.0_weight0.1_warmup0_maxlen128
 # find best checkpoint on dev in ${OUTPUT_DIR}/train_log.txt
-BEST_CKPT_DEV=${OUTPUT_DIR}/epoch=8.ckpt
+BEST_CKPT_DEV=${OUTPUT_DIR}/epoch=12.ckpt
 PYTORCHLIGHT_HPARAMS=${OUTPUT_DIR}/lightning_logs/version_0/hparams.yaml
-GPU_ID=0,1
+GPU_ID=0
 
 python3 ${REPO_PATH}/evaluate/mrc_ner_evaluate.py ${BEST_CKPT_DEV} ${PYTORCHLIGHT_HPARAMS} ${GPU_ID}
