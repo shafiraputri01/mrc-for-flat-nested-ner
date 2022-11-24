6745027a81bc70f175ef4bff9d308eab4fe89115
Shafira Putri Novia Hartanti <79043591+shafiraputri01@users.noreply.github.com>
2022-11-24 18:11:00 +0700

    Create evaluate_conll03_test.sh

---
+++ scripts/mrc_ner/evaluate_conll03_test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# file: eval.sh
+
+REPO_PATH=/content/mrc-for-flat-nested-ner
+export PYTHONPATH="$PYTHONPATH:$REPO_PATH"
+
+OUTPUT_DIR=/content/drive/MyDrive/outputs/outputs/mrc_ner/0901/conll03_cased_large_cased_large_lr4e-5_drop0.3_norm_weight0.1_warmup0_maxlen
+# find best checkpoint on dev in ${OUTPUT_DIR}/train_log.txt
+BEST_CKPT_DEV=${OUTPUT_DIR}/epoch=14.ckpt
+PYTORCHLIGHT_HPARAMS=${OUTPUT_DIR}/lightning_logs/version_0/hparams.yaml
+GPU_ID=0
+
+python3 ${REPO_PATH}/evaluate/mrc_ner_evaluate.py ${BEST_CKPT_DEV} ${PYTORCHLIGHT_HPARAMS} ${GPU_ID}
